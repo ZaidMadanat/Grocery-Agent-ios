4bb3f6794d3ed48a01fc1a28024f569281c78bb5
Zaid Madanat <zaidsmadanat@gmail.com>
2025-10-25 03:03:24 -0700

    Initial Commit

---
+++ Grocery Agent/Grocery_AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_AgentApp.swift
+//  Grocery Agent
+//
+//  Created by Zaid Madanat on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
